33ecd7dc3e3c64927a2f9f5a26f9a97e677ebfeb
6410110118 <6410110118@psu.ac.th>
2023-12-27 17:26:18 +0700

    first-commit

---
+++ Assets/TwoTVGames/DayNightSystem2D/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+    public Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+        Animate();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float AnimationMoveX = Input.GetAxisRaw("Horizontal");
+        float AnimationMoveY = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(AnimationMoveX, AnimationMoveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+    void Animate()
+    {
+        Anim.SetFloat("AnimationMoveX", moveDirection.x);
+        Anim.SetFloat("AnimationMoveY", moveDirection.y);
+    }
+}
